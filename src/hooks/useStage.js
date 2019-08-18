a1739782743c7cf037ba93ebaaa2e43190e637bf
George Ciurescu <shubst3r@gmail.com>
2019-08-18 21:32:56 +0100

    updateds on gameplay

---
+++ src/hooks/useStage.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+import { createStage } from '../gameHelpers';
+
+export const useStage = (player, resetPlayer) => {
+  const [stage, setStage] = useState(createStage());
+
+  useEffect(() => {
+    const updateStage = prevStage => {
+      // first flush the stage
+      const newStage = prevStage.map(row =>
+        row.map(cell => ([1] === 'clear' ? [0, 'clear'] : cell)),
+    );
+  };
+
+    setStage(prev => updateStage(prev))
+
+  }, [])
+
+  return [stage, setStage]
+}
